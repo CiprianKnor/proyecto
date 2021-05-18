de13b71db8720f98e105c855321ad0031ec4d3f3
CiprianKnor <knor.ciprian06@gmail.com>
2021-05-18 22:57:45 +0100

    role_id migration

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'id' => '1',
+            'name' => 'admin',
+            'email' => 'admin@dws.es',
+            'password' => bcrypt('secret'),
+            'role_id' => 1
+        ]);
+
         $count = 100;
         factory(User::class, $count)->create();
     }
